9587d29e3579d41440a80a1c3085ad5291db4c23
Maarten Fremouw <maarten@fremouw.nl>
2016-08-09 20:25:35 +0200

    updated to store a JSON string to EEPROM, this adds more flexibility.

--- src/mfr_configuration.cpp
+++ src/mfr_configuration.cpp
@@ -1,4 +1,69 @@
 #include "mfr_configuration.h"
+#include <Arduino.h>
 
 namespace mfr {
+void Configuration::clear() {
+    Serial.println("Configuration::clear()");
+
+    for (int i = 0; i < kEEPROMSize; ++i) {
+        EEPROM.write(i, 0x0);
+    }
+
+    EEPROM.commit();
+
+    EEPROM.end();
+}
+
+void Configuration::loadConfig() {
+    Serial.println("Configuration::loadConfig()");
+
+    EEPROM.begin(kEEPROMSize);
+
+    if ((EEPROM.read(0) == _MFR_IDENTIFIER[0]) &&
+        (EEPROM.read(1) == _MFR_IDENTIFIER[1]) &&
+        (EEPROM.read(2) == _MFR_IDENTIFIER[2])) {
+        for (size_t t = _MFR_CONFIGURATION_START; t < sizeof(config); t++) {
+            *((char *)&config + t) = EEPROM.read(t);
+        }
+    } else {
+        Serial.println("No Config.");
+    }
+
+    JsonObject& _root = jsonBuffer.parseObject(config.jsonString);
+
+    for (auto & it : _root) {
+        // We create a copy of the key/value pair.
+        root.set(String(it.key), String(it.value.asString()));
+    }
+}
+
+void Configuration::saveConfig() {
+    //
+    memset(config.jsonString, 0x0, sizeof(config.jsonString));
+
+    //
+    root.printTo(config.jsonString, sizeof(config.jsonString));
+
+    //
+    size_t size = (sizeof(config) - sizeof(config.jsonString)) + strlen(
+        config.jsonString);
+
+    for (size_t j = 0; j < size; j++) {
+        EEPROM.write(j, *((char *)&config + j));
+    }
+
+    EEPROM.commit();
+
+    EEPROM.end();
+}
+
+void Configuration::get(const String& key, String& value) {
+    const String v = root[key];
+
+    value = v;
+}
+
+void Configuration::set(const String& key, const String& value) {
+    root[key] = String(value);
+}
 }
--- src/mfr_configuration.h
+++ src/mfr_configuration.h
@@ -1,6 +1,38 @@
+#include <ArduinoJson.h>
+#include <EEPROM.h>
+
+#ifndef MFR_CONFIGURATION_H_
+#define MFR_CONFIGURATION_H_
+
+#define _MFR_IDENTIFIER "MF1"
 
 namespace mfr {
-class Configuration {
+        class Configuration {
+public:
+                Configuration() : root(jsonBuffer.createObject()) {
+                };
 
-};
+                void clear();
+                void loadConfig();
+                void saveConfig();
+
+                void get(const String& key, String& value);
+                void set(const String& key, const String& value);
+
+private:
+                static constexpr size_t kEEPROMSize = 512;
+
+                struct eepromLayout {
+                        char id[4];
+                        char jsonString[kEEPROMSize - 4];
+                } config = {
+                        _MFR_IDENTIFIER,
+                        ""
+                };
+
+                StaticJsonBuffer<kEEPROMSize> jsonBuffer;
+                JsonObject& root;
+        };
 }
+
+#endif // MFR_CONFIGURATION_H_
